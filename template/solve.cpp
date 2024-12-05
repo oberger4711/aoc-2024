7ba42b0f713cb891e8cdf927270d066dc8577bc1
oberger <ole.k.berger@t-online.de>
2024-12-05 07:49:48 +0100

    Parse file once in template

--- template/solve.cpp
+++ template/solve.cpp
@@ -3,22 +3,30 @@
 #include <iostream>
 #include <string>
 
-void solvePart1() {
-  std::ifstream ifs("input.txt");
+std::vector<std::string> parseFile(std::ifstream &ifs) {
   std::vector<std::string> lines;
   std::string line;
+  int row = 0;
   while (std::getline(ifs, line)) {
     lines.push_back(line);
   }
+  return lines;
 }
 
-void solvePart2() {
+void solvePart1(const std::vector<std::string> &lines) {
+  // TODO
+}
+
+void solvePart2(const std::vector<std::string> &lines) {
   // TODO
 }
 
 int main() {
-  measureTime(&solvePart1, "Part 1");
-  measureTime(&solvePart2, "Part 2");
+  // std::ifstream ifs("input.txt");
+  std::ifstream ifs("input_test.txt");
+  const auto lines = parseFile(ifs);
+  measureTime([&lines]() { solvePart1(lines); }, "Part 1");
+  measureTime([&lines]() { solvePart2(lines); }, "Part 2");
   std::cout << "Done.\n";
   return 0;
 }
